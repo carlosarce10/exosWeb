a90199ce7f64e00ce9993057411132e69d3f6687
carlo <carlosarce800@gmail.com>
2020-11-07 12:15:21 -0600

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="q-pt-xl bg-white">
+        <img
+          class="q-ml-xl"
+          src="../assets/logo.png"
+          style="height: 60px; max-width: 140px"
+        />
+        <q-space />
+        <q-tabs shrink>
+          <q-tab class="letras" name="tab1" label="Nosotros"></q-tab>
+          <q-tab class="letras" name="tab1" label="Experiencia"></q-tab>
+          <q-tab class="letras" name="tab1" label="Producto"></q-tab>
+          <q-tab class="letras" name="tab1" label="Servicios"></q-tab>
+          <q-tab class="letras" name="tab1" label="Contacto"></q-tab>
+        </q-tabs>
+      </q-toolbar>
+    </q-header>
+  </q-layout>
+</template>
+<script>
+import "../css/estilos.css";
+export default {
+  name: "MainLayout",
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md">
+      <h1>holas</h1>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
