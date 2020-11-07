073ed2059d9e55b71c9226586101004fbe3f8245
carlo <carlosarce800@gmail.com>
2020-11-07 15:12:45 -0600

    Áreas de experiencia

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,38 +1,63 @@
 <template>
-<div class="q-pa-md">
   <div>
-    <q-carousel animated v-model="slide" navigation infinite class="carrusel">
-      <q-carousel-slide :name="1" img-src="../assets/banPrin.png" />
-    </q-carousel>
-  </div>
-
-   <div class="row" id="nosotros">
-     <div class="col-12 col-md-4 offset-md-2">
-       <h3>Acerca</h3> 
-       <h3>de nosotros</h3>
-     </div>
-     <div class="col-12 col-md-6" >
-       <p>Nos constituímos como una sociedad en 2016 con el propósito de</p>
-       <p>ofrecer servicios especializados de <b>medios de pago, consultoría y</b></p>
-       <p><b>desarrollo de software.</b></p>
-
-       <p>Contamos con <b>más de 25 años de experiencia laboral,</b> trabajando</p>
-       <p>con Bancos, Instituciones Financieras y Fintechs, Nacionales e</p>
-       <p>Internacionales</p>
-
-       <p>Formamos un equipo <b>multidisciplinario</b> con experiencias en diversos</p>
-       <p>ámbitos tales como Medios de pago, TI, Operaciones, Negociación y</p>
-       <p>Relaciones entre participantes en el mercado mexicano</p>
-     </div>
-      
+    <div>
+      <q-carousel animated v-model="slide" navigation infinite class="carrusel">
+        <q-carousel-slide :name="1" img-src="../assets/banPrin.png" />
+      </q-carousel>
     </div>
-    <div class="row" id="clientes">
-      <div class="col-12 col-md-12">
-        <h4>Nuestros clientes</h4>
-        <img src="../assets/clientes.png" alt="">
+
+    <div class="row nosotros">
+      <div class="col-12 col-md-4 offset-md-2">
+        <h3>Acerca</h3>
+        <h3>de nosotros</h3>
+      </div>
+      <div class="col-12 col-md-6">
+        <p>Nos constituímos como una sociedad en 2016 con el propósito de</p>
+        <p>
+          ofrecer servicios especializados de
+          <b>medios de pago, consultoría y</b>
+        </p>
+        <p><b>desarrollo de software.</b></p>
+
+        <p>
+          Contamos con <b>más de 25 años de experiencia laboral,</b> trabajando
+        </p>
+        <p>con Bancos, Instituciones Financieras y Fintechs, Nacionales e</p>
+        <p>Internacionales</p>
+
+        <p>
+          Formamos un equipo <b>multidisciplinario</b> con experiencias en
+          diversos
+        </p>
+        <p>ámbitos tales como Medios de pago, TI, Operaciones, Negociación y</p>
+        <p>Relaciones entre participantes en el mercado mexicano</p>
       </div>
     </div>
-</div>
+    <div class="row clientes q-pb-xl">
+      <div class="col-12 col-md-12">
+        <h4>Nuestros clientes</h4>
+        <img src="../assets/clientes.png" alt="" />
+      </div>
+    </div>
+    <div class="row imgExp">
+      <div class="col-10 col-4-md">
+        <h3>Áreas de</h3>
+        <h3>experiencia</h3>
+      </div>
+      <div class="col-10 col-4-md">
+        <h3>Somos expertos</h3>
+      </div>
+      <div>
+        <li>Integración a la banca mexicana para emisores / adquirentes</li>
+        <li>Reglamentación local / ABM / Banxico</li>
+        <li>
+          Análisis y construcción de interfaces especialidades en la banca
+        </li>
+        <li>Cuota de intercambio</li>
+        <li>Minería de datos y análisis transaccional</li>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
