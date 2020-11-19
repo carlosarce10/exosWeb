be4f589564916332ac904d32f19c2d8fbde263ed
itsviejon <dcalflo8@gmail.com>
2020-11-18 22:25:24 -0600

    Correcciones

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,34 +11,62 @@
     </div>
 
     <!--ACERCA DE NOSOTROS-->
-    <div class="row nosotros" id="nosotros">
-      <div class="col-12 col-md-4 offset-md-2">
-        <h3>Acerca</h3>
-        <h3>de nosotros</h3>
+    <div class="nosotros" id="nosotros">
+      <div class="row gt-sm">
+        <div class="col-12 col-md-4 offset-md-2">
+          <h3>Acerca</h3>
+          <h3>de nosotros</h3>
+        </div>
+        <div class="col-12 col-md-6">
+          <p>Nos constituímos como una sociedad en 2016 con el propósito de</p>
+          <p>
+            ofrecer servicios especializados de
+            <b>medios de pago, consultoría y</b>
+          </p>
+          <p><b>desarrollo de software.</b></p>
+
+          <p>
+            Contamos con <b>más de 25 años de experiencia laboral,</b> trabajando
+          </p>
+          <p>con Bancos, Instituciones Financieras y Fintechs, Nacionales e</p>
+          <p>Internacionales</p>
+
+          <p>
+            Formamos un equipo <b>multidisciplinario</b> con experiencias en
+            diversos
+          </p>
+          <p>ámbitos tales como Medios de pago, TI, Operaciones, Negociación y</p>
+          <p>Relaciones entre participantes en el mercado mexicano</p>
+        </div>
       </div>
-      <div class="col-12 col-md-6">
-        <p>Nos constituímos como una sociedad en 2016 con el propósito de</p>
-        <p>
-          ofrecer servicios especializados de
-          <b>medios de pago, consultoría y</b>
-        </p>
-        <p><b>desarrollo de software.</b></p>
+      <div class="lt-md q-ml-xl q-pl-xl">
+        <div class="col-12">
+          <h3>Acerca</h3>
+          <h3>de nosotros</h3>
+        </div>
+        <div class="col-12">
+          <p>Nos constituímos como una sociedad en 2016 con el propósito de</p>
+          <p>
+            ofrecer servicios especializados de
+            <b>medios de pago, consultoría y</b>
+          </p>
+          <p><b>desarrollo de software.</b></p>
 
-        <p>
-          Contamos con <b>más de 25 años de experiencia laboral,</b> trabajando
-        </p>
-        <p>con Bancos, Instituciones Financieras y Fintechs, Nacionales e</p>
-        <p>Internacionales</p>
+          <p>
+            Contamos con <b>más de 25 años de experiencia laboral,</b> trabajando
+          </p>
+          <p>con Bancos, Instituciones Financieras y Fintechs, Nacionales e</p>
+          <p>Internacionales</p>
 
-        <p>
-          Formamos un equipo <b>multidisciplinario</b> con experiencias en
-          diversos
-        </p>
-        <p>ámbitos tales como Medios de pago, TI, Operaciones, Negociación y</p>
-        <p>Relaciones entre participantes en el mercado mexicano</p>
+          <p>
+            Formamos un equipo <b>multidisciplinario</b> con experiencias en
+            diversos
+          </p>
+          <p>ámbitos tales como Medios de pago, TI, Operaciones, Negociación y</p>
+          <p>Relaciones entre participantes en el mercado mexicano</p>
+        </div>
       </div>
     </div>
-
     <!--NUESTROS CLIENTES-->
     <div class="row clientes q-pb-xl" id="#clientes">
       <div class="col-12 col-md-12">
@@ -70,11 +98,11 @@
     </div>
 
     <!-- EXPERIENCIAS DIGITALES-->
-    <div class="row clientes q-pb-xl">
+    <div class="row clientes">
       <div class="col-12 experiencias">
         <h4>Experiencias digitales e interactivas (UX/UI)</h4>
       </div>
-      <div class="col-12">
+      <div class="q-mb-xl q-ml-xl">
         <q-card flat bordered class="my-card">
           <q-card-section class="q-ml-md">
             <div class="text-h2 titulo q-mt-xl">
@@ -117,7 +145,7 @@
           </q-card-section>
         </q-card>
       </div>
-      <div class="col-12">
+      <div class="q-mb-xl q-ml-xl">
         <q-card flat bordered class="my-card">
           <q-card-section class="q-ml-md">
             <div class="text-h2 titulo q-mt-xl">
@@ -161,9 +189,8 @@
     <div class="row productos" id="productos">
       <h4>Nuestros productos</h4>
     </div>
-    <div class="slider">
-      <div class="q-pt-xl q-pb-xl">
-        <q-option-group color="purple" inline class="q-mb-md" />
+    <div class="row slider">
+      <div class="col q-mt-xl q-mb-xl">
         <q-carousel
           animated
           v-model="slide"
@@ -233,6 +260,7 @@
           </q-tabs>
         </template>
 
+        <div class="gt-md">
         <template>
           <q-tab-panels
             v-model="tab"
@@ -384,6 +412,162 @@
             </q-tab-panel>
           </q-tab-panels>
         </template>
+        </div>
+      </div>
+      <div class="lt-md">
+        <div class="q-mr-xl q-mt-xl">
+          <template>
+          <q-tab-panels
+            v-model="tab"
+            animated
+            swipeable
+            vertical
+            transition-prev="jump-down"
+            transition-next="jump-up"
+            class="color-servicios"
+          >
+            <q-tab-panel name="consultoria">
+              <p>
+                Amplia experiencia en medios de pago y soluciones tecnológicas,
+              </p>
+              <p>
+                operativas y de negocio para la incorporación al mercado
+                mexicano e
+              </p>
+              <p>
+                internacional. Ofrecemos productos y soluciones acordes a los
+              </p>
+              <p>
+                estándares y necesidades apoyados por análisis de Normativas,
+              </p>
+              <p>
+                auditorías PCI, recomendaciones para adecuación de procesos con
+                las
+              </p>
+              <p>marcas (VISA, MASTERCARD, AMEX).</p>
+              <br />
+
+              <p>
+                Contamos con expertos que cuenta con más de
+                <strong>25 años de</strong>
+              </p>
+              <p>
+                <strong>experiencia en la industria</strong> de medios de pago.
+                Es por eso que el
+              </p>
+              <p>
+                enfoque en eXos está orientado a satisfacer las necesidades
+                logísticas,
+              </p>
+              <p>funcionales y tecnológicas del ámbito de medios de pago.</p>
+            </q-tab-panel>
+
+            <q-tab-panel name="desarrollos">
+              <p>
+                Somos expertos en Proyectos de Desarrollo de Software basados en
+              </p>
+              <p>
+                las especificaciones de nuestros clientes, y gracias a nuestra
+              </p>
+              <p>
+                experiencia acumulada desde hace mas de 25 años en la industria
+                de
+              </p>
+              <p>
+                medios de pago entregamos al cliente Software, sin defectos,
+              </p>
+              <p>
+                proyectos a tiempo y la mejor relación de costo-beneficio. A
+                través de
+              </p>
+              <p>
+                nuestra metodología apoyamos a nuestros clientes a incrementar
+                su
+              </p>
+              <p>
+                capacidad de producción de proyectos de desarrollo
+                permitiéndoles
+              </p>
+              <p>concentrarse en el core de su negocio.</p>
+
+              <p>
+                Dominamos el desarrollo de aplicaciones en JAVA, .NET,
+                Aplicaciones
+              </p>
+              <p>Móviles y tecnologías emergentes.</p>
+            </q-tab-panel>
+
+            <q-tab-panel name="integracion">
+              <p>
+                Las nuevas tecnologías continúan revolucionando el sector
+                bancario.
+              </p>
+              <p>
+                Los nuevos participantes en el mercado digital incrementan la
+              </p>
+              <p>
+                competencia en mercados ya saturados. Los clientes esperan
+                obtener
+              </p>
+              <p>
+                servicios en línea innovadores pero también de la mano con los
+              </p>
+              <p>
+                productos existentes y en el marco de las normativas actuales.
+              </p>
+              <p>
+                Integramos productos y desarrollos inmersos en la operación
+                actual
+              </p>
+              <p>normada con énfasis en mejorar la experiencia del cliente y</p>
+              <p>optimización de la interacción entre plataformas.</p>
+            </q-tab-panel>
+
+            <q-tab-panel name="backoffice">
+              <p>
+                En un contexto cada vez más competitivo y complicado como el
+                actual,
+              </p>
+              <p>
+                las empresas aprovechan todos los recursos posibles para
+                disminuir
+              </p>
+              <p>
+                costos, mejorar la organización interna y ofrecer un servicio
+                basado en
+              </p>
+              <p>
+                la excelencia. Un ejemplo concreto de esta realidad es
+                <strong>Back Office</strong>, un
+              </p>
+              <p>
+                conjunto de tareas que engloba una serie de actividades de
+                carácter
+              </p>
+              <p>
+                operativo y administrativo y que son imprescindibles para
+                asegurar el
+              </p>
+              <p>correcto funcionamiento de la corporación.</p>
+              <br />
+
+              <p>
+                Ofrecemos servicios de gestión que ayudan a mejorar la
+                productividad
+              </p>
+              <p>
+                de las empresas optimizando al máximo los recursos disponibles
+                con la
+              </p>
+              <p>ayuda de personal altamente capacitado y con herramientas</p>
+              <p>
+                tecnológicas que permiten monitorear, controlar y asegurar la
+              </p>
+              <p>estabilidad de las operaciones.</p>
+            </q-tab-panel>
+          </q-tab-panels>
+        </template>
+        </div>    
       </div>
     </div>
     <!--PRODUCTOS-->
